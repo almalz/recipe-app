e956145cf98278ec01ecd3a3810fc0eee66d832c
alexismalzieu <alex.malzieu@gmail.com>
2020-12-13 13:54:14 +0100

    🔥 remove useless story lines

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,11 +2,3 @@ module.exports = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials']
 }
-// module.exports = {
-//   webpackFinal: async (baseConfig) => {
-//     const nextConfig = require('../../next.config.js')
-
-//     // merge whatever from nextConfig into the webpack config storybook will use
-//     return { ...baseConfig }
-//   }
-// }
--- src/components/button/Button.stories.tsx
+++ src/components/button/Button.stories.tsx
@@ -1,20 +1,8 @@
 // import React from 'react'
 // // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
-import { Story, Meta } from '@storybook/react/types-6-0'
+import { Story } from '@storybook/react/types-6-0'
 
 import { Button, ButtonProps } from './Button'
-
-// export const Primary: React.VFC<{}> = () => (
-//   <Button primary label="Primary"></Button>
-// )
-
-// export default {
-//   title: 'Example/Button',
-//   component: Button,
-//   argTypes: {
-//     backgroundColor: { control: 'color' }
-//   }
-// } as Meta
 
 export default {
   title: 'Example/Button',
@@ -28,26 +16,3 @@ Primary.args = {
   primary: true,
   label: 'Button'
 }
-
-// export const Secondary = Template.bind({})
-// Secondary.args = {
-//   label: 'Button'
-// }
-
-// export const Large = Template.bind({})
-// Large.args = {
-//   size: 'large',
-//   label: 'Button'
-// }
-
-// export const Small = Template.bind({})
-// Small.args = {
-//   size: 'small',
-//   label: 'Button'
-// }
-
-// export const Primary: React.VFC<{}> = () => (
-//   <Button primary label="Button">
-//     Button
-//   </Button>
-// )
