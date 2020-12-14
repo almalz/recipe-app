96d8d72aa72a1ef04275d9f8c05dfb9dd8a9774c
alexismalzieu <alex.malzieu@gmail.com>
2020-12-14 22:03:38 +0100

    🔥 remove unused theme config

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import tw from 'twin.macro'
 
 import StyledButton from './Button.styles'
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,8 @@
 import { AppProps } from 'next/app'
 import 'tailwindcss/tailwind.css'
-import { ThemeProvider } from 'styled-components'
-import theme from '../assets/themes'
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  )
+  return <Component {...pageProps} />
 }
 
 export default App
